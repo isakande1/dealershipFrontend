3cdc02ce92bf9168cc0306c3a3e9cc33465c4389
ae458 <ae458@njit.edu>
2024-04-04 18:23:14 -0400

    fix css

--- client/src/App.js
+++ client/src/App.js
@@ -938,14 +938,23 @@ const OwnCar = () => {
         </li>
       </ul>
     </nav>
-      <Box
-        bg='black'
-        w='100%'
-        color='white'
-        height='100%'
-        bgGradient="linear(to-b, black, gray.600)"
-        p={4}
-      >
+    <Box
+          bgGradient="linear(to-b, black, gray.600)"
+          position='fixed'
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          zIndex='-1'
+        />
+        <Box
+          w='100%'
+          color='white'
+          p={1}
+          css={{ margin: 0, padding: 0 }}
+          height='100vh'
+          overflowY='scroll' // Enable vertical scrolling
+        >
         <Flex justifyContent="space-between" alignItems="center">
           <Text fontSize="3xl" fontWeight="bold">Add Own Car</Text>
         </Flex>
