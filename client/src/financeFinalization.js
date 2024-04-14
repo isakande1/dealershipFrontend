73b533085001264bb6cf3349e49bb11827216c87
eugenegraves <elg22@njit.edu>
2024-04-13 23:06:20 -0400

    Fixing info migration to finalization page

--- client/src/financeFinalization.js
+++ client/src/financeFinalization.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link, useParams, useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 
 export default function FinalizeFinance () {
+    const location = useLocation();
+    const { financeTerms, userData, carInfos } = location.state;
+    console.log(financeTerms);
+    console.log(userData);
+    console.log(carInfos);
     <div>
         
     </div>
